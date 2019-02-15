1367eea0804bd9420ec3fd13c9269508437440f3
Zachary Jeffreys <zachary.jeffreys@wsu.edu>
2019-02-14 18:30:05 -0800

    Update slidesFunctions.js

--- slidesFunctions.js
+++ slidesFunctions.js
@@ -1,3 +1,4 @@
+//-----------------------Dog Registery slider
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -15,6 +16,35 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
+//---------------Buztle Sldier-------------------
+var iOSslideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(iOSslideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("iosSlides");
+  var dots = document.getElementsByClassName("iosdot");
   if (n > slides.length) {slideIndex = 1} 
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
